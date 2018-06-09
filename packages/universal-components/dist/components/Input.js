de171c2b71c1486040e5015a963ad1be99cad8ab
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-06-09 22:06:42 +0200

    sketch out universal component

---
+++ packages/universal-components/dist/components/Input.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = function _default() {
+  return _react.default.createElement("div", null, "Test");
+};
+
+exports.default = _default;
