21d9e456bbdf2146571b37afd367e55c430860a1
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-06-09 22:22:31 +0200

    use react-primitives

--- packages/universal-components/src/components/Input.js
+++ packages/universal-components/src/components/Input.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { View } from 'react-primitives';
 
-export default () => (<div>Test!</div>);
+export default () => (<View>Test!</View>);
