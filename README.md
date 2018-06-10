69c3db7f393c11fbc5386f5ef03ec4399aebea72
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-06-10 13:54:50 +0200

    use yarn

--- README.md
+++ README.md
@@ -3,14 +3,16 @@
 ## add package
 
  - create folder in `packages`
- - run `npm init`, set private field
+ - run `npm init` or `yarn init`, set private field
 
 ## bootstrap
 To install all packages deps and run their prepublish (builds universal comp):
-- `npm run lerna -- bootstrap`
+- `npm run lerna -- bootstrap` or `yarn lerna bootstrap`
 
 ## add one project to another
- - `npm run lerna -- add srs-universal-components --scope=native-app`
+ - `npm run lerna -- add srs-universal-components --scope=native-app` or `yarn lerna add srs-universal-components --scope=native-app`
 
 ## run script in all packages
- - `npm run lerna -- run start`
+ - `npm run lerna -- run start` or `yarn lerna run start`
+
+ read: https://medium.com/viewsdx/how-to-use-yarn-workspaces-with-create-react-app-and-create-react-native-app-expo-to-share-common-ea27bc4bad62
--- packages/native-app/App.js
+++ packages/native-app/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Input } from './dist';
-// import { Input as Input2 } from 'srs-universal-components';
+// import { Input } from './dist';
+import { Input as Input2 } from 'srs-universal-components';
 // does not work imported right now
 
 export default class App extends React.Component {
@@ -11,8 +11,8 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <Input title="test" subtitle="1" />
-        {/* <Input2 title="test2" subtitle="1" /> */}
+        {/* <Input title="test" subtitle="1" /> */}
+        <Input2 title="test2" subtitle="1" />
       </View>
     );
   }
