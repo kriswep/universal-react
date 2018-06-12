7070d885b09c8fdd5e3a50c2923d48ed23027332
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-06-12 14:04:35 +0200

    add input example

--- packages/universal-components/dist/components/Input.js
+++ packages/universal-components/dist/components/Input.js
@@ -4,11 +4,49 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
 var _reactNative = require('react-native');
 
-Object.defineProperty(exports, 'default', {
-  enumerable: true,
-  get: function get() {
-    return _reactNative.TextInput;
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Input = function Input(props) {
+  return _react2.default.createElement(
+    _reactNative.View,
+    { style: [styles.container, props.style] },
+    _react2.default.createElement(_reactNative.TextInput, _extends({}, props, { style: styles.input }))
+  );
+};
+
+var styles = _reactNative.StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    backgroundColor: '#ffffff',
+    justifyContent: 'space-between',
+    maxWidth: '100%',
+    overflow: 'hidden',
+    padding: 16
+  },
+  input: {
+    backgroundColor: '#eee',
+    color: '#000000',
+    borderColor: '#eee',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 2,
+    padding: 8,
+    fontSize: 16
   }
 });
+
+exports.default = Input;
--- packages/universal-components/src/components/Input.js
+++ packages/universal-components/src/components/Input.js
@@ -1,1 +1,34 @@
-export { TextInput as default } from 'react-native';
+import React from 'react';
+import propTypes from 'prop-types';
+import { StyleSheet, View, TextInput } from 'react-native';
+
+const Input = (props) => (
+  <View style={[styles.container, props.style]}>
+    <TextInput {...props} style={styles.input} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    backgroundColor: '#ffffff',
+    justifyContent: 'space-between',
+    maxWidth: '100%',
+    overflow: 'hidden',
+    padding: 16,
+  },
+  input: {
+    backgroundColor: '#eee',
+    color: '#000000',
+    borderColor: '#eee',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 2,
+    padding: 8,
+    fontSize: 16,
+  },
+});
+
+export default Input;
