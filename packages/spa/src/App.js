3b33e6048732715a18f51c100f0177b5fe3238e1
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-06-13 15:59:21 +0200

    add prettier

--- packages/spa/src/App.js
+++ packages/spa/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          <Header title="test" subtitle="!"/>
+          <Header title="test" subtitle="!" />
           <Input placeholder="..." />
         </p>
       </div>
