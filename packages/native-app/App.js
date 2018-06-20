3aca4f6e73818002a33821ffc700f13e0d47fbe3
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-06-20 13:30:15 +0200

    remove unused CRNA boilerplate

--- packages/native-app/App.js
+++ packages/native-app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, ScrollView, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 import { Header, Input } from './universal-components';
 
 const styles = StyleSheet.create({
@@ -8,7 +8,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 40,
   },
   scrollView: {
     alignSelf: 'stretch',
@@ -19,26 +18,12 @@ const styles = StyleSheet.create({
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <SafeAreaView style={styles.container}>
         <ScrollView style={styles.scrollView}>
           <Header title="test" subtitle="!" />
           <Input placeholder="1" />
-          <Input placeholder="2" />
-          <Input placeholder="3" />
-          <Input placeholder="4" />
-          <Input placeholder="5" />
-          <Input placeholder="6" />
-          <Input placeholder="7" />
-          <Input placeholder="8" />
-          <Input placeholder="9" />
-          <Input placeholder="10" />
-          <Input placeholder="11" />
-          <Input placeholder="12" />
         </ScrollView>
-      </View>
+      </SafeAreaView>
     );
   }
 }
